8778457e73d159a6f201016378721b23190d611a
Chris Bedwell <ckbedwell@gmail.com>
2022-09-27 10:08:23 +0100

    Added a container component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
+import React from 'react'
 import { Main } from '~/Components/Main'
+import { Container } from '~/Components/Container'
 
 import styles from './App.module.css'
 
 export function App() {
   return (
-    <div className={styles.App}>
-      <div className={styles.nested}>
-        Check that nesting works
+    <Container>
+      <div className={styles.App}>
+        <div className={styles.nested}>
+          Check that nesting works
+        </div>
+        <Main />
       </div>
-      <Main />
-    </div>
+    </Container>
   )
 }
